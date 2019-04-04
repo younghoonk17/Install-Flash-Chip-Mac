8e1a30128149fe41131ff41954aa0474420d2410
Thore-Krug <36414231+Thore-Krug@users.noreply.github.com>
2019-04-04 18:50:14 +0200

    Update Install-Chip-Dependencies.sh

--- Install-Chip-Dependencies.sh
+++ Install-Chip-Dependencies.sh
@@ -2,18 +2,10 @@
 
 sudo /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" -y
 
-brew install bash wget curl screen u-boot-tools
+brew install bash wget curl screen u-boot-tools libusb zlib 
 wget https://raw.githubusercontent.com/Homebrew/homebrew-cask/cf82f0a10d08afd146e176c275a03964ae9e5866/Casks/android-platform-tools.rb
 
 brew cask install android-platform-tools.rb
-
-cd /usr/local/bin
-
-wget https://github.com/Thore-Krug/Sunxi-tools-Mac/archive/V.1.0.zip
-
-unzip V.1.0.zip
-
-rm V.1.0.zip
 
 cd
 
@@ -23,8 +15,14 @@ cd Chip-tools-Mac
 
 find ./ -name "*.sh" -exec chmod +x {} \;
 
-cd
+git clone https://github.com/linux-sunxi/sunxi-tools.git
 
-wget https://github.com/Thore-Krug/Chip-tools-Mac/releases/download/V1/Flash-CHIP-Mac
+cd sunxi-tools 
+
+sudo make tools 
+
+sudo make install 
+
+cd
 
 echo " U can now Close this Window "
