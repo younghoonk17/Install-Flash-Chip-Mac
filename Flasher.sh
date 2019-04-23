8121606f78d7cdf1bc0ce49ef9eb633332ee55be
Thore-Krug <36414231+Thore-Krug@users.noreply.github.com>
2019-04-23 14:37:19 +0200

    Update Flasher.sh

--- Flasher.sh
+++ Flasher.sh
@@ -6,7 +6,7 @@ function install-brew {
 
   sudo /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" -y
 
-  brew install bash wget curl screen u-boot-tools libusb zlib pkg-config
+  brew install bash wget curl screen u-boot-tools libusb zlib pkg-config git 
   wget https://raw.githubusercontent.com/Homebrew/homebrew-cask/cf82f0a10d08afd146e176c275a03964ae9e5866/Casks/android-platform-tools.rb
 
   brew cask install android-platform-tools.rb
